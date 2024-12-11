8fb4f5d2e043cb01a61e77750d107c26f3a5ff00
Matheus De Oliveira Rodrigues Saleh <100796455+MatheusSaleh@users.noreply.github.com>
2024-12-11 15:47:14 -0300

    feat: consumir backend

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,8 @@
 import { View, Text } from "react-native"
+import { router } from "expo-router";
 
 import { Welcome } from "@/components/welcome"
+import { Button } from "@/components/button"
 import { Steps } from "@/components/steps"
 
 export default function Index(){
@@ -8,6 +10,12 @@ export default function Index(){
     <View style={{flex: 1, padding: 40, gap: 40}}>
       <Welcome />
       <Steps/>
+      <Button onPress={() => {
+        router.navigate("/home")
+      }}>
+        <Button.Title>Começar</Button.Title>
+        
+      </Button>
     </View>
   )
 }
