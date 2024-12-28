a69a02e0d64ff467f8e9e9598091dfa8c57e5e82
Matheus De Oliveira Rodrigues Saleh <100796455+MatheusSaleh@users.noreply.github.com>
2024-12-28 07:59:03 -0300

    commit final

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,60 +1,141 @@
 import { useEffect, useState } from "react";
-import { View, Alert } from "react-native";
+import { View, Alert, Text } from "react-native";
+import MapView, { Callout, Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 import { api } from "@/services/api";
+import { fontFamily, colors } from "@/styles/theme";
 
 import { Places } from "@/components/places";
 import { PlaceProps } from "@/components/place";
 import { Categories, CategoriesProps } from "@/components/categories";
-//parei em 01:27:13
+import { router } from "expo-router";
 
-type MarketsProps = PlaceProps 
+type MarketsProps = PlaceProps & {
+  latitude: number;
+  longitude: number;
+};
+
+const currentLocation = {
+  latitude: -23.55052,
+  longitude: -46.633308,
+};
 
 export default function Home() {
-
   const [categories, setCategories] = useState<CategoriesProps>([]);
-  const [category, setCategory ] = useState("")
-  const [markets, setMarkets] = useState<MarketsProps[]>()
+  const [category, setCategory] = useState("");
+  const [markets, setMarkets] = useState<MarketsProps[]>();
 
-  async function fetchCategories(){
+  async function fetchCategories() {
     try {
-      const { data } = await api.get("/categories")
-      setCategories(data)
-      setCategory(data[0].id)
-    } catch (error){
-      console.log(error)
-      Alert.alert("Categorias", "Não foi possível carregar as categorias")
+      const { data } = await api.get("/categories");
+      setCategories(data);
+      setCategory(data[0].id);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Categorias", "Não foi possível carregar as categorias");
     }
   }
 
   async function fetchMarkets() {
     try {
-      if(!category){
-        return
+      if (!category) {
+        return;
       }
 
-
-      const { data } = await api.get("/markets/category/" + category)
-      setMarkets(data)
-      console.log(data)
+      const { data } = await api.get("/markets/category/" + category);
+      setMarkets(data);
+      console.log(data);
+      console.log(data);
     } catch (error) {
-      console.log(error)
-      Alert.alert("Locais", "Não foi possível carregar os locais.")
+      console.log(error);
+      Alert.alert("Locais", "Não foi possível carregar os locais.");
+    }
+  }
+
+  async function getCurrentLocation() {
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+
+      if (granted) {
+        const location = await Location.getCurrentPositionAsync({});
+        currentLocation.latitude = location.coords.latitude;
+        currentLocation.longitude = location.coords.longitude;
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Localização", "Não foi possível obter a localização atual.");
     }
   }
 
   useEffect(() => {
-    fetchCategories()
-  }, [])
+    getCurrentLocation();
+    fetchCategories();
+  }, []);
 
   useEffect(() => {
-    fetchMarkets()
-  }, [category])
-
+    fetchMarkets();
+  }, [category]);
   return (
-    <View style={{flex: 1, backgroundColor: "#CECECE"}}>
-      <Categories data={categories} onSelect={setCategory} selected={category}/>
-      <Places data={markets || []}/>
+    <View style={{ flex: 1, backgroundColor: "#CECECE" }}>
+      <Categories
+        data={categories}
+        onSelect={setCategory}
+        selected={category}
+      />
+
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+        <Marker
+          identifier="current"
+          coordinate={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+          }}
+          image={require("@/assets/location.png")}
+        />
+
+        {markets?.map((item) => (
+          <Marker
+            key={item.id}
+            coordinate={{
+              latitude: item.latitude,
+              longitude: item.longitude,
+            }}
+            image={require("@/assets/pin.png")}
+          >
+            <Callout onPress={() => {router.navigate(`/market/${item.id}`)}}>
+              <View>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.medium,
+                  }}
+                >
+                  {item.name}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.regular,
+                  }}
+                >{item.address}</Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
+
+      <Places data={markets || []} />
     </View>
-  )
+  );
 }
--- src/components/places/index.tsx
+++ src/components/places/index.tsx
@@ -1,22 +1,23 @@
-import { Text, useWindowDimensions } from "react-native";
-import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet";
+import { useRef } from "react"
+import { Text, useWindowDimensions } from "react-native"
+import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet"
+import { router } from "expo-router"
 
-import {s} from "./styles";
-import { Place, PlaceProps } from "../place";
-import { useRef } from "react";
+import { s } from "./styles"
+import { Place, PlaceProps } from "../place"
 
 type Props = {
-  data: PlaceProps[];
-};
+  data: PlaceProps[]
+}
 
 export function Places({ data }: Props) {
-  const dimensions = useWindowDimensions();
-  const bottomSheetRef = useRef<BottomSheet>(null);
+  const dimensions = useWindowDimensions()
+  const bottomSheetRef = useRef<BottomSheet>(null)
 
   const snapPoints = {
     min: 278,
     max: dimensions.height - 128,
-  };
+  }
 
   return (
     <BottomSheet
@@ -29,13 +30,18 @@ export function Places({ data }: Props) {
       <BottomSheetFlatList
         data={data}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Place data={item} />}
+        renderItem={({ item }) => (
+          <Place
+            data={item}
+            onPress={() => {router.navigate(`/market/${item.id}`)}}
+          />
+        )}
         contentContainerStyle={s.content}
-        ListHeaderComponent={
-          <Text style={s.title}>Explore Locais perto de você</Text>
-        }
+        ListHeaderComponent={() => (
+          <Text style={s.title}>Explore locais perto de você</Text>
+        )}
         showsVerticalScrollIndicator={false}
       />
     </BottomSheet>
-  );
+  )
 }
